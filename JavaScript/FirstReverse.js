fd9224ffc4b3c751da00083cea4e0a564512fcb7
Dave McCalmont <dave_mccalmont@hotmail.com>
2019-03-28 23:34:33 -0500

    Setting up repo for completed JS challenges

---
+++ JavaScript/FirstReverse.js
@@ -0,0 +1,14 @@
+
+/*"Have the function FirstReverse(str) take the str parameter being passed and
+return the string in reversed order. For example: if the input string is
+"Hello World and Coders" then your program should return the string
+sredoC dna dlroW olleH."*/
+
+//My solution:
+function FirstReverse(str) {
+
+  var wordArray = Array.from(str).reverse();
+  var word = wordArray.join('');
+  return word;
+
+}
